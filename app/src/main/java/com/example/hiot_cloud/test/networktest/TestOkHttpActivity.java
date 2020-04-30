574483f93c91fe7044dcc43995a0d61f5143c21f
chenmingqing <806595986@qq.com>
2020-04-30 16:12:20 +0800

    主页UI完善和启动页面制作

---
+++ app/src/main/java/com/example/hiot_cloud/test/networktest/TestOkHttpActivity.java
@@ -0,0 +1,16 @@
+package com.example.hiot_cloud.test.networktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hiot_cloud.R;
+
+public class TestOkHttpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_ok_http);
+    }
+}
